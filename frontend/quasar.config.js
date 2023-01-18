7a843f13e2aa094b1adf2ca4e998b4cdd90531b1
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2023-01-18 08:37:46 +0100

    add scripts, activate pwa

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -138,7 +138,7 @@ module.exports = configure(function (/* ctx */) {
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
 
-      pwa: false,
+      pwa: true,
 
       // manualStoreHydration: true,
       // manualPostHydrationTrigger: true,
