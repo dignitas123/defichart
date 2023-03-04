73534dba114f24fed858dcdf10f539682ea9cbd9
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2023-03-04 14:47:20 +0100

    refac(charts): more timedisplay functions in composable

--- frontend/src/components/chart-window/child-components/candlestick-chart/candlestick-chart.cp.ts
+++ frontend/src/components/chart-window/child-components/candlestick-chart/candlestick-chart.cp.ts
@@ -1,10 +1,154 @@
+import { Ref } from 'vue';
 import { DATE_BOX_WIDTH } from 'src/pages/broker-charts/consts';
-import { TimeMode, TimeModePeriod } from './candlestick-chart.if';
+import { useLanguageStore } from 'src/stores/language';
+import {
+  TimeDisplayProperties,
+  TimeMode,
+  TimeModePeriod,
+} from './candlestick-chart.if';
+import { format as dateFormat } from 'date-fns';
+import { DatePositionEntry } from 'src/pages/broker-charts/broker-charts.if';
 
-export function useTimeDisplay(
+export function useDateFunctions(
   width: number | undefined,
-  dates: Date[] | undefined
+  dates: Date[] | undefined,
+  timeDisplayProps: Ref<TimeDisplayProperties | undefined>,
+  xPositionCandlestick: Ref<number>,
+  candleWidth: Ref<number>,
+  datePositionEntries: Ref<DatePositionEntry[]>
 ) {
+  const languageStore = useLanguageStore();
+
+  function formatDate(date: Date, format: string) {
+    return dateFormat(date, format, { locale: languageStore.language });
+  }
+
+  function addDateToDatePositionEntries(date: Date, index: number) {
+    if (!timeDisplayProps.value) {
+      return;
+    }
+    let formattedDate = '';
+    let bold = false;
+    let format = '';
+
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+
+    if (timeDisplayProps.value.period === TimeModePeriod.Minute) {
+      format = 'HH:mm';
+      if (minutes === 0 && hours === 0) {
+        format = 'dd.MM';
+      }
+      if (minutes % timeDisplayProps.value.timeDifferential === 0) {
+        if (timeDisplayProps.value.mode === TimeMode.M5 && minutes % 15 === 0) {
+          bold = true;
+        } else if (
+          timeDisplayProps.value.mode === TimeMode.M10 &&
+          minutes % 30 === 0
+        ) {
+          bold = true;
+        } else if (
+          timeDisplayProps.value.mode === TimeMode.M15 &&
+          minutes === 0
+        ) {
+          bold = true;
+        } else if (
+          timeDisplayProps.value.mode === TimeMode.M30 &&
+          date.getHours() % 2 === 0 &&
+          minutes === 0
+        ) {
+          bold = true;
+        }
+        formattedDate = formatDate(date, format);
+      }
+    } else if (timeDisplayProps.value.period === TimeModePeriod.Hour) {
+      format = 'HH:mm';
+      if (minutes % 60 === 0) {
+        if (hours === 0) {
+          format = 'dd.MM';
+          bold = true;
+        }
+        if (timeDisplayProps.value.mode === TimeMode.H1) {
+          if (hours % 4 === 0) {
+            bold = true;
+          }
+          formattedDate = formatDate(date, format);
+        } else if (timeDisplayProps.value.mode === TimeMode.H3) {
+          if (hours % 9 === 0) {
+            bold = true;
+          }
+          if (hours % 3 === 0) {
+            formattedDate = formatDate(date, format);
+          }
+        } else if (timeDisplayProps.value.mode === TimeMode.H6) {
+          if (hours % 12 === 0) {
+            bold = true;
+          }
+          if (hours % 6 === 0) {
+            formattedDate = formatDate(date, format);
+          }
+        }
+      }
+    } else if (timeDisplayProps.value.period === TimeModePeriod.Day) {
+      const days = date.getDate();
+      format = 'dd.MM';
+      if (days % timeDisplayProps.value.timeDifferential === 0) {
+        if (timeDisplayProps.value.mode === TimeMode.W2) {
+          if (days % 15 === 0 && days !== 30) {
+            formattedDate = formatDate(date, format);
+          }
+        } else {
+          formattedDate = formatDate(date, format);
+        }
+        if (days === 1) {
+          format = 'MM';
+          formattedDate = formatDate(date, format);
+        }
+        if (date.getMonth() === 1) {
+          format = 'YYYY';
+          formattedDate = formatDate(date, format);
+          bold = true;
+        }
+      }
+    } else if (timeDisplayProps.value.period === TimeModePeriod.Month) {
+      const month = date.getMonth();
+      format = 'MM';
+      if (month % timeDisplayProps.value.timeDifferential === 0) {
+        if (month === 1) {
+          bold = true;
+          format = 'YYYY';
+        }
+        formattedDate = formatDate(date, format);
+      }
+    } else if (timeDisplayProps.value.period === TimeModePeriod.Year) {
+      format = 'YYYY';
+      if (date.getFullYear() % 10 === 0) {
+        bold = true;
+      }
+      formattedDate = formatDate(date, format);
+    }
+
+    const xPosition = xPositionCandlestick.value + candleWidth.value / 2;
+    if (!formattedDate) {
+      datePositionEntries.value.push({
+        index: index,
+        x: xPosition,
+        date: formatDate(date, format),
+        bold: bold,
+        show: false,
+      });
+      return;
+    }
+
+    datePositionEntries.value.push({
+      index: index,
+      x: xPosition,
+      date: formattedDate,
+      bold: bold,
+      show: true,
+    });
+  }
+
   const MIN = 1000 * 60;
   const HOUR = MIN * 60;
   const DAY = HOUR * 24;
@@ -101,5 +245,6 @@ export function useTimeDisplay(
 
   return {
     timeDisplayProperties,
+    addDateToDatePositionEntries,
   };
 }
--- frontend/src/components/chart-window/child-components/candlestick-chart/candlestick-chart.vue
+++ frontend/src/components/chart-window/child-components/candlestick-chart/candlestick-chart.vue
@@ -42,18 +42,11 @@
 
 <script lang="ts" setup>
 import { computed, onMounted, ref, watch } from 'vue';
-import { format as dateFormat } from 'date-fns';
-import {
-  Candle,
-  TimeDisplayProperties,
-  TimeMode,
-  TimeModePeriod,
-} from './candlestick-chart.if';
+import { Candle, TimeDisplayProperties } from './candlestick-chart.if';
 import {
   DatePositionEntry,
   OHLC,
 } from 'src/pages/broker-charts/broker-charts.if';
-import { useLanguageStore } from 'src/stores/language';
 import {
   CANDLE_BEAR_COLOR,
   CANDLE_BORDER,
@@ -61,12 +54,11 @@ import {
   CANDLE_BULL_COLOR,
   CANDLE_WICK_THICKNESS,
   DATA_TICKSIZE,
-  DATE_BOX_WIDTH,
   GRID_LINES_TRANSPARENCY,
 } from 'src/pages/broker-charts/consts';
 import CandleStick from './child-components/candle-stick.vue';
 import { roundToTicksize } from '../../helpers/digits';
-import { useTimeDisplay } from './candlestick-chart.cp';
+import { useDateFunctions } from './candlestick-chart.cp';
 
 const props = withDefaults(
   defineProps<{
@@ -119,12 +111,6 @@ watch(candleDistance, () => {
 });
 
 const candles = ref<Candle[]>([]);
-
-const languageStore = useLanguageStore();
-
-function formatDate(date: Date, format: string) {
-  return dateFormat(date, format, { locale: languageStore.language });
-}
 
 function calcCandleXDistance(cW: number) {
   const increase = CANDLE_BORDER ? 1 : 0;
@@ -188,132 +174,6 @@ function drawChart() {
     addDateToDatePositionEntries(ohlc.d, index);
     lastXPositionCandlestick.value = xPositionCandlestick.value;
     xPositionCandlestick.value += candleWidth.value + candleDistance.value;
-  });
-}
-
-function addDateToDatePositionEntries(date: Date, index: number) {
-  if (!timeDisplayProps.value) {
-    return;
-  }
-  let formattedDate = '';
-  let bold = false;
-  let format = '';
-
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-
-  if (timeDisplayProps.value.period === TimeModePeriod.Minute) {
-    format = 'HH:mm';
-    if (minutes === 0 && hours === 0) {
-      format = 'dd.MM';
-    }
-    if (minutes % timeDisplayProps.value.timeDifferential === 0) {
-      if (timeDisplayProps.value.mode === TimeMode.M5 && minutes % 15 === 0) {
-        bold = true;
-      } else if (
-        timeDisplayProps.value.mode === TimeMode.M10 &&
-        minutes % 30 === 0
-      ) {
-        bold = true;
-      } else if (
-        timeDisplayProps.value.mode === TimeMode.M15 &&
-        minutes === 0
-      ) {
-        bold = true;
-      } else if (
-        timeDisplayProps.value.mode === TimeMode.M30 &&
-        date.getHours() % 2 === 0 &&
-        minutes === 0
-      ) {
-        bold = true;
-      }
-      formattedDate = formatDate(date, format);
-    }
-  } else if (timeDisplayProps.value.period === TimeModePeriod.Hour) {
-    format = 'HH:mm';
-    if (minutes % 60 === 0) {
-      if (hours === 0) {
-        format = 'dd.MM';
-        bold = true;
-      }
-      if (timeDisplayProps.value.mode === TimeMode.H1) {
-        if (hours % 4 === 0) {
-          bold = true;
-        }
-        formattedDate = formatDate(date, format);
-      } else if (timeDisplayProps.value.mode === TimeMode.H3) {
-        if (hours % 9 === 0) {
-          bold = true;
-        }
-        if (hours % 3 === 0) {
-          formattedDate = formatDate(date, format);
-        }
-      } else if (timeDisplayProps.value.mode === TimeMode.H6) {
-        if (hours % 12 === 0) {
-          bold = true;
-        }
-        if (hours % 6 === 0) {
-          formattedDate = formatDate(date, format);
-        }
-      }
-    }
-  } else if (timeDisplayProps.value.period === TimeModePeriod.Day) {
-    const days = date.getDate();
-    format = 'dd.MM';
-    if (days % timeDisplayProps.value.timeDifferential === 0) {
-      if (timeDisplayProps.value.mode === TimeMode.W2) {
-        if (days % 15 === 0 && days !== 30) {
-          formattedDate = formatDate(date, format);
-        }
-      } else {
-        formattedDate = formatDate(date, format);
-      }
-      if (days === 1) {
-        format = 'MM';
-        formattedDate = formatDate(date, format);
-      }
-      if (date.getMonth() === 1) {
-        format = 'YYYY';
-        formattedDate = formatDate(date, format);
-        bold = true;
-      }
-    }
-  } else if (timeDisplayProps.value.period === TimeModePeriod.Month) {
-    const month = date.getMonth();
-    format = 'MM';
-    if (month % timeDisplayProps.value.timeDifferential === 0) {
-      if (month === 1) {
-        bold = true;
-        format = 'YYYY';
-      }
-      formattedDate = formatDate(date, format);
-    }
-  } else if (timeDisplayProps.value.period === TimeModePeriod.Year) {
-    format = 'YYYY';
-    if (date.getFullYear() % 10 === 0) {
-      bold = true;
-    }
-    formattedDate = formatDate(date, format);
-  }
-
-  const xPosition = xPositionCandlestick.value + candleWidth.value / 2;
-  if (!formattedDate) {
-    datePositionEntries.value.push({
-      index: index,
-      x: xPosition,
-      date: formatDate(date, format),
-      bold: bold,
-      show: false,
-    });
-    return;
-  }
-
-  datePositionEntries.value.push({
-    index: index,
-    x: xPosition,
-    date: formattedDate,
-    bold: bold,
-    show: true,
   });
 }
 
@@ -432,7 +292,15 @@ watch(
   { deep: true }
 );
 
-const { timeDisplayProperties } = useTimeDisplay(props.width, props.dates);
+const { timeDisplayProperties, addDateToDatePositionEntries } =
+  useDateFunctions(
+    props.width,
+    props.dates,
+    timeDisplayProps,
+    xPositionCandlestick,
+    candleWidth,
+    datePositionEntries
+  );
 
 onMounted(async () => {
   drawChart();
