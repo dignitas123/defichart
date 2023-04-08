9524991c41c7a0b4180a6e52b42fa916f1f9cf5b
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2023-04-08 08:33:21 +0200

    feat(backend): alchemy price data tick stream

---
+++ backend/alchemy/get-price.js
@@ -0,0 +1,60 @@
+import { getReceipt } from "./get-receipt.js";
+import { decodeReceiptSignature, getSwapResultData } from "./log-data-fns.js";
+
+/**
+ * Default values are for BTC.
+ * The function can return the same timestamp multile times. It should be checked if it's the same as
+ * before, because volume should be added in this case
+ *
+ * @param { string } txHash transaction hash
+ * @param { BigInt } divider divider to get the right conversion from resultdata to f.e. btc value,
+ * @param { BigInt } decimals 100000n f.e. is 8 digits
+ * @returns
+ */
+export async function getPriceData(
+  txHash,
+  divider = 1000000000000000000n,
+  decimals = 8
+) {
+  if (!txHash) {
+    console.log("noTxHash");
+    return undefined;
+  }
+
+  const receiptResult = await getReceipt(txHash);
+
+  if (!receiptResult) {
+    console.error("noReceiptResult");
+    return undefined;
+  }
+
+  const decodedLogResult = decodeReceiptSignature(receiptResult);
+
+  if (!decodedLogResult) {
+    console.error("noDecodedLogResult");
+    return undefined;
+  }
+
+  const swapResultData = getSwapResultData(decodedLogResult);
+
+  if (swapResultData.length < 2) {
+    console.error("swapResultData.length < 2");
+    return undefined;
+  }
+
+  // to replace Math.abs() which doesn't work for BigInt
+  const abs = (n) => (n === -0 || n < 0n ? -n : n);
+
+  const divideBigInts = (a, b, decimals) => {
+    const result = (a * BigInt(10 ** decimals)) / b;
+    return Number(result) / 10 ** decimals;
+  };
+
+  const currentTime = new Date();
+  return {
+    volume: divideBigInts(BigInt(abs(swapResultData[0])), divider, decimals),
+    direction: swapResultData[0] > 0,
+    price: abs(swapResultData[1]) / abs(swapResultData[0]),
+    timestamp: currentTime.getTime(),
+  };
+}
