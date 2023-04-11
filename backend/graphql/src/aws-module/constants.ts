0b8f8ceb6e4a385b58edd6d01b3c3c7b6184fbb2
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2023-04-11 21:08:20 +0200

    feat(graphql): kinesis shard example added

--- backend/graphql/src/aws-module/constants.ts
+++ backend/graphql/src/aws-module/constants.ts
@@ -1,4 +1,8 @@
 export const constants = {
   DATABASE_NAME: "defichartTickDatabase",
   TABLE_NAME: "btcusd-perp",
+  KINESIS_STREAM_NAME: "defichartsSymbols",
+  KINESIS_CONSUMER_ARN:
+    "arn:aws:kinesis:eu-central-1:234291048405:stream/defichartsSymbols",
+  REGION: "eu-central-1",
 };
