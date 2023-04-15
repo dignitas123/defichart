5865e157cae555afb611d9b6a94c8ae570f394bf
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2023-04-15 21:29:55 +0200

    feat(data-transformer): init

---
+++ backend/data-transformer/README.md
@@ -0,0 +1,10 @@
+# Excel formatting
+
+## make a new column 'date' with
+DATE(1970,1,1)+E2/86400000
+
+## format to see data
+TT.MM.JJJJ hh:mm:ss
+
+## apply a filter to columns to sort data
+Now you can delete data column again and export as .csv
---
+++ backend/data-transformer/perp-listcandlestick-example.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+
+// data to be retrieved from graphql queries on app.perp.com
+const d1 = {
+  "data": {
+    "listCandleSticks": {
+      "items": [
+        {
+          "market": "0x86f1e0420c26a858fc203A3645dD1A36868F18e5",
+          "resolution": "5m",
+          "startTime": 1680572400,
+          "open": "27850.9736040537288201949727074843667957208481",
+          "high": "27850.9736040537288201949727074843667957208481",
+          "low": "27850.9736040537288201949727074843667957208481",
+          "close": "27850.9736040537288201949727074843667957208481",
+          "volume": "0",
+          "baseAssetVol": "0",
+          "txCount": 0,
+          "blockNumber": 85983700,
+          "version": 0,
+          "__typename": "CandleStick"
+        }
+      ],
+      "__typename": "CandleStickConnection"
+    }
+  }
+}
+
+const res = []
+d1.data.listCandleSticks.items.forEach(item => {
+  res.push({high: item.high, low: item.low, close: item.close, volume: Number(item.volume), timestamp: item.startTime * 1000});
+})
+// d2 etc. (you have to name the single requests with list candlesticks items each)
+
+const headers = Object.keys(res[0]).join(',');
+const rows = res.filter(obj => obj.volume > 0).map(obj => Object.values(obj).join(','));
+
+const csv = `${headers}\n${rows.join('\n')}`;
+
+fs.writeFile('btcusd-perp_m5.csv', csv, err => {
+  if (err) throw err;
+  console.log('CSV file has been created successfully.');
+});
