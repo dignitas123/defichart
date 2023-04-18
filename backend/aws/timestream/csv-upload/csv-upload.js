95992756f58e6333c4c05391f300581882f02062
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2023-04-18 08:41:14 +0200

    fix(timestream): fix csv upload

--- backend/aws/timestream/csv-upload/csv-upload.js
+++ backend/aws/timestream/csv-upload/csv-upload.js
@@ -1,7 +1,7 @@
 import { candleSticksStreamWrite } from "../write-data.js";
 import csv from "csvtojson";
 
-const csvFilePath = "BTCUSD_d1_toy.csv"; // make sure files are formatted like the toy dataset
+const csvFilePath = "BTCUSD_d1.csv"; // make sure files are formatted like the toy dataset
 
 async function readCSVFileFrom(csvFilePath) {
   return await csv().fromFile(csvFilePath);
@@ -10,7 +10,8 @@ async function readCSVFileFrom(csvFilePath) {
 const hlcvtRecords = await readCSVFileFrom(csvFilePath);
 
 // Define the size of each chunk
-const chunkSize = 2;
+// AWS chunks for timestream should be 20 when putting 4 records into the database (100 is maximum record insert)
+const chunkSize = 20;
 
 const chunks = [];
 for (let i = 0; i < hlcvtRecords.length; i += chunkSize) {
@@ -18,9 +19,9 @@ for (let i = 0; i < hlcvtRecords.length; i += chunkSize) {
 }
 
 console.log("Process each chunk one by one");
-chunks.forEach((chunks, index) => {
+for (let i = 0; i < chunks.length; i++) {
   setTimeout(async () => {
-    console.log(`Processing chunk ${index + 1} chunk length ${chunks.length}`);
-    await candleSticksStreamWrite(chunks, "testTable");
-  }, 3000);
-});
+    console.log(`Processing chunk ${i + 1} chunk length ${chunks[i].length}`);
+    await candleSticksStreamWrite(chunks[i], "btcusd-perp_d1");
+  }, i * 1000);
+}
