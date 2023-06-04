c7a786e3ee34af3017f3c72d0a0ec72843432958
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2023-06-05 00:00:44 +0200

    refac(layout): time component refac interface, functionality

--- frontend/src/components/atomic-time/atomic-time.vue
+++ frontend/src/components/atomic-time/atomic-time.vue
@@ -7,34 +7,40 @@
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import { WorldTimeResult } from './atomic-time.if';
 
-export default {
-  setup() {
-    const formattedTime = ref('');
+const time = ref<Date>();
 
-    const getAtomicTime = () => {
-      axios.get('https://worldtimeapi.org/api/ip').then((response) => {
-        const currentTime = new Date(response.data.utc_datetime);
-        formattedTime.value = currentTime
-          .toISOString()
-          .split('T')[1]
-          .slice(0, 8);
-      });
-    };
+const formattedTime = ref('');
 
-    onMounted(() => {
-      getAtomicTime();
-      setInterval(getAtomicTime, 1000); // Update the time every second
+function formatTime(time: Date) {
+  return time.toISOString().split('T')[1].slice(0, 8);
+}
+
+function updateAtomicTime() {
+  if (!time.value) {
+    return;
+  }
+  time.value.setSeconds(time.value.getSeconds() + 1);
+  formattedTime.value = formatTime(time.value);
+}
+
+function getAtomicTime() {
+  axios
+    .get('https://worldtimeapi.org/api/ip')
+    .then((result: WorldTimeResult) => {
+      time.value = new Date(result.data.utc_datetime);
+      formattedTime.value = formatTime(time.value);
     });
+}
 
-    return {
-      formattedTime,
-    };
-  },
-};
+onMounted(() => {
+  getAtomicTime();
+  setInterval(updateAtomicTime, 1000); // Update the time every second
+});
 </script>
 
 <style lang="scss" scoped>
